3a2a8c94784bc700eb8c4cd07cd049d1c866daaf
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2024-10-23 09:59:47 -0300

    finish teste criar empresa

--- tests/testes_empresa.py
+++ tests/testes_empresa.py
@@ -2,14 +2,11 @@ import unittest
 
 from statsmodels.tools.testing import assert_equal
 
+from src.empresa import Empresa
+
 
 class TestEmpresa(unittest.TestCase):
     def test_criar_empresa(self):
         nome = "Bradesco"
         empresa = Empresa(nome)
         assert_equal(empresa.nome, nome)
-
-
-
-
-unittest.main()
